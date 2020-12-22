04b505b48700863660607066ac4f0232450b3e0d
wangyuxiang <wangyuxiang985@gmail.com>
2020-12-22 22:14:26 +0800

    vscode自动编译js代码

---
+++ typescript/02_autoChange.ts
@@ -0,0 +1,8 @@
+(()=>{
+
+    function autoChange(str:string){
+        return "我自动转换了：" + str;
+    }
+
+    console.log(autoChange("hello world"))
+})()
---
+++ typescript/js/02_autoChange.js
@@ -0,0 +1,6 @@
+(function () {
+    function autoChange(str) {
+        return "我自动转换了：" + str;
+    }
+    console.log(autoChange("hello world"));
+})();
