7a01f5aaff4538dcc3beea84f1044faeac846d46
wangyuxiang <wangyuxiang985@gmail.com>
2021-01-03 23:03:12 +0800

    声明文件

---
+++ typescript/webPack/src/01_声明文件及内置函数.ts
@@ -0,0 +1,32 @@
+(()=>{
+/* 
+当使用第三方库时，我们需要引用它的声明文件，才能获得对应的代码补全、接口提示等功能。
+声明语句: 如果需要ts对新的语法进行检查, 需要要加载了对应的类型说明代码
+  declare var jQuery: (selector: string) => any;
+声明文件: 把声明语句放到一个单独的文件（jQuery.d.ts）中, ts会自动解析到项目中所有声明文件
+下载声明文件: npm install @types/jquery --save-dev
+*/
+
+/*
+内置对象
+内置对象是指根据标准在全局作用域（Global）上存在的对象。这里的标准是指 ECMAScript 和其他环境（比如 DOM）的标准。
+
+ECMAScript 的内置对象
+Boolean
+Number
+String
+Date
+RegExp
+Error
+
+BOM 和 DOM 的内置对象
+Window
+Document
+HTMLElement
+DocumentFragment
+Event
+NodeList
+*/
+
+
+})()
